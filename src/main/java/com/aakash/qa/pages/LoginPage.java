493b84eea7d0ef03ce1b25f56f7103aba450005f
Arun Kumar Pandey <010645@Corpdomain.com>
2020-04-03 22:15:21 +0530

    code for checkout and login

--- src/main/java/com/aakash/qa/pages/LoginPage.java
+++ src/main/java/com/aakash/qa/pages/LoginPage.java
@@ -1,7 +1,48 @@
 package com.aakash.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.aakash.qa.base.TestBase;
 
 public class LoginPage extends TestBase {
 
+	
+	@FindBy(xpath="//input[@id='edit-name' and @name='name']")
+	
+	WebElement loginField;
+	
+	
+	@FindBy(xpath="//input[@id='edit-pass' and @name='pass']")
+	
+	WebElement passwordfield;
+	
+	
+	
+	@FindBy(xpath="//input[@id='edit-submit' and @value='Sign In']")
+	
+	WebElement signIN;
+	
+	
+	public LoginPage() {
+		
+		
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void login(String username, String password) {
+		
+		loginField.sendKeys(username);
+		
+		passwordfield.sendKeys(password);
+		
+		signIN.click();
+		
+		
+		//return new StudentDashBoard();
+		
+	}
+	
 }
---
+++ src/test/java/com/aakash/qa/testcases/LoginPageTest.java
@@ -0,0 +1,72 @@
+package com.aakash.qa.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aakash.qa.base.TestBase;
+import com.aakash.qa.pages.CartPage;
+import com.aakash.qa.pages.CoursesPage;
+import com.aakash.qa.pages.HomePage;
+import com.aakash.qa.pages.LoginPage;
+import com.aakash.qa.pages.SearchedCoursePage;
+import com.aakash.qa.utill.DataProviderClass;
+import com.aakash.qa.utill.TestUtilAakash;
+
+public class LoginPageTest extends TestBase {
+
+	HomePage homePage;
+	CoursesPage coursesPage;
+	TestUtilAakash aakashutil;
+	static String sheetName1 = "Eng";
+	SearchedCoursePage searchedPage;
+	CartPage cartpage;
+	CartPageTest cartpagetets;
+	LoginPage loginpage;
+
+	public LoginPageTest() throws InterruptedException {
+
+		super();
+
+	}
+
+	@BeforeMethod
+	public void setup() throws InterruptedException {
+
+		initialaztion();
+
+		homePage = new HomePage();
+		aakashutil = new TestUtilAakash();
+		aakashutil.switchframe();
+		homePage.clickOnCrossIcon();
+		aakashutil.defaultframe();
+		homePage.clickOnCourseLink();
+		coursesPage = new CoursesPage();
+		coursesPage.clickonEngineering();
+		searchedPage = new SearchedCoursePage();
+		cartpage = new CartPage();
+		loginpage = new LoginPage();
+	}
+	
+	
+
+	@Test(priority = 1, dataProvider = "getEngineeringData", dataProviderClass = DataProviderClass.class)
+	public void validateLoginPage(String chooseClass, String chooseState, String chooseCenters)
+			throws InterruptedException {
+
+		coursesPage.selectEngineeringcourses(chooseClass, chooseState, chooseCenters);
+		coursesPage.clickOnSearchedCourses();
+		searchedPage.clickOnPayRegistration();
+		cartpage.clickOnProceedToCheckOut();
+		loginpage.login(prop.getProperty("user_name"), prop.getProperty("password"));
+	}
+
+	@AfterMethod
+
+	public void tearDown() {
+
+		driver.quit();
+
+	}
+
+}
